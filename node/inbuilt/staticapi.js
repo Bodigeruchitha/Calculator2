9553786b069fd9078aabcecdb0a06f27f2af6cf8
Bodigeruchitha <bodigeruchitha123@gmail.com>
2023-06-06 22:47:54 +0530

    Initial commit

---
+++ node/inbuilt/staticapi.js
@@ -0,0 +1,15 @@
+let http= require("http");
+let fs= require("fs");
+let port=2367;
+
+let server=http.createServer((req,res)=>{
+    fs.readFile("myloc.json","utf-8",(err,data)=>{
+        if (err) throw err;
+        res.write(data);
+        res.end()
+    })
+})
+server.listen(port)
+
+
+
